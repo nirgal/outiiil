fd43a5d00101f1459b08b2999d09ff08dd5287a7
Jean-Michel Vourgère <jmv_deb@nirgal.com>
2013-05-19 01:21:04 +0200

    Add GPL license header
    GPL license is granted as describe at
    https://userscripts.org/scripts/show/160493

--- outiiil_1.1.user.js
+++ outiiil_1.1.user.js
@@ -11,6 +11,24 @@
 // @version        1.1
 // ==/UserScript==
 
+/*
+    outiiil - A greasemonkey extension for fourmizzz game
+    Copyright (C) 2013 Hraesvleg
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 style = " \
 #centre { \
 padding-top: 35px !important; \
